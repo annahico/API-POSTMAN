0a72c6d5a79c7a8ccebbaf75522b4d5121e68dfa
annahico <annahico@gmail.com>
2024-04-24 19:50:37 +0200

    modificando codigo JS

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-fetch("https://dummyjson.com/users")
-    .then(response => response.json())
-    .then(data => console.log(data));
+const btnGetUsers = document.getElementById("get-users");
+const loading = document.getElementById("loading");
+const root = document.getElementById("root");
+
+btnGetUsers.addEventListener("click", () => {
+    getAllUsers(); // llamar a todos los usuarios
+});
+
+const getAllUsers = async () => {
+    try {
+        await delay(2000); //OJO eliminar en producción
+        loading.textContent = "Loading...";
+
+        const response = await fetch("https://dummyjson.com/users");
+        const data = await response.json();
+        const users = data.users;
+        console.log(users);
+    } catch (error) {
+        console.log(error);
+    } finally {
+        loading.textContent = "";
+    }
+};
+
